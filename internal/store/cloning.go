15938a05b12110d9fbc967033e35866ca6797313
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for dd37b4ae-2e93-4a74-9fbe-939d2c72a50e-bug-2

--- internal/store/cloning.go
+++ internal/store/cloning.go
@@ -6,5 +6,18 @@ func cloneTrial(value domain.GerminationTrial) domain.GerminationTrial {
 	value.ObservationSchedule = append([]string(nil), value.ObservationSchedule...)
 	value.Observations = append([]domain.Observation(nil), value.Observations...)
 	value.Issues = append([]domain.Issue(nil), value.Issues...)
+	value.EvidenceNotes = append([]domain.EvidenceNote(nil), value.EvidenceNotes...)
+	value.RemediationItems = append([]domain.RemediationItem(nil), value.RemediationItems...)
+	value.Review = cloneReview(value.Review)
+	value.ReviewHistory = append([]domain.Review(nil), value.ReviewHistory...)
+	for i := range value.ReviewHistory {
+		value.ReviewHistory[i] = cloneReview(value.ReviewHistory[i])
+	}
+	value.DesignSummary.PlannedStages = append([]string(nil), value.DesignSummary.PlannedStages...)
+	return value
+}
+
+func cloneReview(value domain.Review) domain.Review {
+	value.RemediationItems = append([]domain.RemediationItem(nil), value.RemediationItems...)
 	return value
 }
