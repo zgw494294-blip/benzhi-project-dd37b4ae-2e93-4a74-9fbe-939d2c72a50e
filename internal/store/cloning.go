3968d5ffdebf9d1a6914845257fdec124dbd27c0
Benzhi Generation Client <benzhi-generation-client@users.noreply.github.com>
2026-08-25 17:06:24 +0800

    Seed bug/slot-2-trial-review-alias

--- internal/store/cloning.go
+++ internal/store/cloning.go
@@ -6,13 +6,5 @@ func cloneTrial(value domain.GerminationTrial) domain.GerminationTrial {
 	value.ObservationSchedule = append([]string(nil), value.ObservationSchedule...)
 	value.Observations = append([]domain.Observation(nil), value.Observations...)
 	value.Issues = append([]domain.Issue(nil), value.Issues...)
-	value.ReviewHistory = append([]domain.Review(nil), value.ReviewHistory...)
-	value.EvidenceNotes = append([]domain.EvidenceNote(nil), value.EvidenceNotes...)
-	value.RemediationItems = append([]domain.RemediationItem(nil), value.RemediationItems...)
-	value.DesignSummary.PlannedStages = append([]string(nil), value.DesignSummary.PlannedStages...)
-	value.Review.RemediationItems = append([]domain.RemediationItem(nil), value.Review.RemediationItems...)
-	for index := range value.ReviewHistory {
-		value.ReviewHistory[index].RemediationItems = append([]domain.RemediationItem(nil), value.ReviewHistory[index].RemediationItems...)
-	}
 	return value
 }
